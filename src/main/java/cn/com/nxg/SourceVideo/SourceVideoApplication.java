5ecc4726de0953e590193cf13dd9780b0f40c798
mynxg <mynxg@qq.com>
2024-09-28 21:44:05 +0800

    init project

---
+++ src/main/java/cn/com/nxg/SourceVideo/SourceVideoApplication.java
@@ -0,0 +1,11 @@
+package cn.com.nxg.SourceVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceVideoApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(SourceVideoApplication.class,args);
+    }
+}
---
+++ src/main/java/cn/com/nxg/SourceVideo/application/package-info.java
@@ -0,0 +1,1 @@
+package cn.com.nxg.SourceVideo.application;
